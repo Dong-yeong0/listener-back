63bba74e56d8274c209d8102b53d65268d5a9ff7
dong-yeong0 <kevin@beomhoit.com>
2025-02-13 19:42:21 +0900

    Change user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,37 +1,53 @@
-# Create your models here.
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
 
-class User(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, default=None)
-    email = models.EmailField(null=False, default=None)
-    password = models.CharField(max_length=255, null=False, default=None)
+    email = models.EmailField(unique=True, null=False, default=None)
     time_zone = models.CharField(null=True, default=None)
+    token = models.CharField(null=True, default=None)
     last_login = models.DateTimeField(null=True)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
-    USERNAME_FIELD = 'id'
-    REQUIRED_FIELDS = []
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
 
     class Meta:
         db_table = 'user'
         ordering = ['-created_at']
-    
+
     @property
     def is_anonymous(self):
-        return True
+        return False
 
     @property
     def is_authenticated(self):
-        return False
-    
-    def set_password(self, password):
-        self.password = make_password(password)
-        
-    def check_password(self, raw_password):
-        from django.contrib.auth.hashers import check_password
-        return check_password(raw_password, self.password)
-    
+        return True
