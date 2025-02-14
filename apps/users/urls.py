6d9242d7877e91f5a1e7630ec1bcb8732be77290
dong-yeong0 <kevin@beomhoit.com>
2025-02-14 14:27:52 +0900

    Add token check, logout api

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -7,4 +7,6 @@ app_name = 'users'
 urlpatterns = [
     path('sign-up', views.SignUpView.as_view(), name='sign-up'),
     path('login', views.LoginView.as_view(), name='login'),
+    path('logout', views.LogoutView.as_view(), name='logout'),
+    path('check-token', views.TokenCheckView.as_view(), name='check-token')
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,7 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -31,8 +33,39 @@ class LoginView(APIView):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             token = serializer.validated_data
+            user = Token.objects.get(key=token).user
+            user_data = UserSerializer(user).data
             return Response(
-                {'status': True, 'token': token}, status=status.HTTP_200_OK
+                {'status': True, 'token': token, 'user': user_data}, status=status.HTTP_200_OK
             )
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TokenCheckView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        token = request.auth
+        if token is not None:
+            try:
+                user = Token.objects.get(key=token).user
+                user_data = UserSerializer(user).data
+                return Response({'status': True, 'user': user_data}, status=status.HTTP_200_OK)
+            except Token.DoesNotExist:
+                return Response({'status': False, 'message': '유효한 토큰이 아닙니다.'}, status=status.HTTP_401_UNAUTHORIZED)
+            
+        return Response({'status': True}, status=status.HTTP_200_OK)
+    
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        token = request.auth
+        user = User.objects.get(token=token)
+        user.token = None
+        user.save()
+        token.delete()
+        return Response({'status': True,}, status=status.HTTP_200_OK)
