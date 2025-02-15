9bc004d76fafb44ba049a11bd9eed8e38dc5ee87
dong-yeong0 <kevin@beomhoit.com>
2025-02-15 18:15:19 +0900

    Change token check, login logic

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,9 +1,5 @@
-from datetime import datetime
-
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers, status
-from rest_framework.authtoken.models import Token
-from tzlocal import get_localzone
 
 from common.exception import (
     CustomException,
@@ -19,28 +15,23 @@ from common.utils import (
 )
 
 from .models import User
+from .models import UserToken as Token
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'name', 'password', 'email', 'time_zone', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'password', 'email', 'device_id', 'token', 'time_zone', 'created_at', 'updated_at')
         extra_kwargs = {
             'password': {'write_only': True}
         }
-    
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        hashed_password = make_password(password)
-        validated_data['password'] = hashed_password
-        return super().create(validated_data)
-    
+
     def validate(self, attrs):
-        if 'email' not in attrs:
+        if not attrs.get('email'):
             raise CustomValidationError(UserValidationMessages.EMAIL_REQUIRED)
-        if 'password' not in attrs:
+        if not attrs.get('password'):
             raise CustomValidationError(UserValidationMessages.PASSWORD_REQUIRED)
-        if 'name' not in attrs:
+        if not attrs.get('name'):
             raise CustomValidationError(UserValidationMessages.NAME_REQUIRED)
         
         return attrs
@@ -67,10 +58,19 @@ class UserSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     email = serializers.CharField(write_only=True, required=False, allow_null=True)
     password = serializers.CharField(write_only=True, required=False, allow_null=True)
+    device_id = serializers.CharField(write_only=True, required=False, allow_null=True)
     
     def validate(self, attrs):
         email = attrs.get('email')
         password = attrs.get('password')
+        device_id = attrs.get('device_id')
+        if not email:
+            raise CustomValidationError(UserValidationMessages.EMAIL_REQUIRED)
+        if not password:
+            raise CustomValidationError(UserValidationMessages.PASSWORD_REQUIRED)
+        if not device_id:
+            raise CustomValidationError(UserValidationMessages.DEVICE_ID_REQUIRED)
+        
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
@@ -78,25 +78,42 @@ class LoginSerializer(serializers.Serializer):
         if not user.check_password(password):
             raise CustomException(LoginErrorMessages.WRONG_EMAIL_OR_PASSWORD, status_code=401)
         
-        token, _ = Token.objects.get_or_create(user=user)
-        user.token = token.key
-        if user.time_zone:
-            local_tz = get_localzone(user.time_zone)
-            user.last_login = datetime.now(local_tz)
-        else:
-            user.last_login = datetime.now(get_localzone())
-        
-        user.save()
-        return token.key
+        attrs['user'] = user
+        return attrs
 
-    def validate_email(self, email):
-        if not email:
-            raise CustomValidationError(UserValidationMessages.EMAIL_REQUIRED)
         
-        return email
+class SignUpSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=False)
+    password = serializers.CharField(write_only=True, required=False)
+    name = serializers.CharField(required=False)
+    device_id = serializers.CharField(write_only=True, required=False)
+    device_os = serializers.CharField(write_only=True, required=False)
+    device_os_version = serializers.CharField(write_only=True, required=False)
+    
+    def validate(self, value):
+        if 'email' not in value:
+            raise CustomValidationError(UserValidationMessages.EMAIL_REQUIRED)
+        if 'password' not in value:
+            raise CustomValidationError(UserValidationMessages.PASSWORD_REQUIRED)
+        if 'name' not in value:
+            raise CustomValidationError(UserValidationMessages.NAME_REQUIRED)
+        
+        return value
+    
+    def validate_email(self, value):
+        if not is_valid_email_format(value):
+            raise CustomValidationError(UserValidationMessages.EMAIL_FORMAT_INVALID)
+        if User.objects.filter(email=value).exists():
+            raise CustomException(message=UserValidationMessages.EMAIL_ALREADY_EXISTS, status_code=status.HTTP_409_CONFLICT)
+
+        return value
     
     def validate_password(self, password):
-        if not password:
-            raise CustomValidationError(UserValidationMessages.PASSWORD_REQUIRED)
-        
+        if not is_password_str_num_included(password):
+            raise CustomValidationError(UserValidationMessages.PASSWORD_STR_NUM_REQUIRED)
+        if not is_valid_password_length(password):
+            raise CustomValidationError(UserValidationMessages.PASSWORD_LENGTH_INVALID)
+        if not is_valid_password_strength(password):
+            raise CustomValidationError(UserValidationMessages.PASSWORD_STRENGTH_INVALID)
+
         return password
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,21 +1,43 @@
+import logging
+
+logger = logging.getLogger("apps")
+
+from datetime import datetime
+
 from rest_framework import status
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from tzlocal import get_localzone
 
-from .models import User
-from .serializers import LoginSerializer, UserSerializer
+# from rest_framework.authentication import TokenAuthentication
+from .authentication import CustomTokenAuthentication as TokenAuthentication
+from .models import User, UserDevice
+from .models import UserToken as Token
+from .serializers import LoginSerializer, SignUpSerializer, UserSerializer
 
 
 class SignUpView(APIView):
     permission_classes = [AllowAny]
     
-    def post(self, request, *args, **kwargs):
-        serializer = UserSerializer(data=request.data)
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            data = serializer.validated_data            
+            # Create user
+            user = User.objects.create(
+                email=data.get('email'),
+                password=data.get('password'),
+                name=data.get('name'),
+                device_id=data.get('device_id'),
+            )
+            # Create user device info
+            UserDevice.objects.create(
+                user=user,
+                device_id=data.get('device_id'),
+                device_os=data.get('device_os'),
+                device_os_version=data.get('device_os_version'),
+            )
             return Response({'created': True}, status=status.HTTP_201_CREATED)
 
         return Response(
@@ -27,18 +49,31 @@ class SignUpView(APIView):
         )
 
 class LoginView(APIView):
+    authentication_classes = []
     permission_classes = [AllowAny]
     
     def post(self, request, *args, **kwargs):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            token = serializer.validated_data
-            user = Token.objects.get(key=token).user
-            user_data = UserSerializer(user).data
-            return Response(
-                {'status': True, 'token': token, 'user': user_data}, status=status.HTTP_200_OK
+            user = serializer.validated_data['user']
+            # Create or replace device info
+            device, _ = UserDevice.objects.update_or_create(
+                user=user,
+                device_id=request.data['device_id'],
             )
-        
+            # Create or replace token by user, device
+            token, _ = Token.get_or_create(user=user, device=device)
+            user.token = token.key
+            if user.time_zone:
+                local_tz = get_localzone(user.time_zone)
+                user.last_login = datetime.now(local_tz)
+            else:
+                user.last_login = datetime.now(get_localzone())
+            
+            user.save()
+            return Response(
+                {'status': True, 'token': token.key}, status=status.HTTP_200_OK
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -50,14 +85,18 @@ class TokenCheckView(APIView):
         token = request.auth
         if token is not None:
             try:
-                user = Token.objects.get(key=token).user
-                user_data = UserSerializer(user).data
-                return Response({'status': True, 'user': user_data}, status=status.HTTP_200_OK)
+                user = UserSerializer(token.user).data                
+                if user.get("token") != token.key:
+                    logger.debug(f"User(email: {user.get('email')}, id: {user.get('id')}) token changed: {token.key} -> {user.get('token')}")
+                    user.update(token=None)
+                    token.delete()
+                    return Response({'status': False, 'device_changed': True, 'message': '로그인 기기가 변경 되었습니다.\n다시 로그인 해주세요.'}, status=status.HTTP_401_UNAUTHORIZED)
+                
+                return Response({'status': True, 'user': user}, status=status.HTTP_200_OK)
             except Token.DoesNotExist:
                 return Response({'status': False, 'message': '유효한 토큰이 아닙니다.'}, status=status.HTTP_401_UNAUTHORIZED)
             
-        return Response({'status': True}, status=status.HTTP_200_OK)
-    
+        return Response({'status': False}, status=status.HTTP_200_OK)
 
 class LogoutView(APIView):
     authentication_classes = [TokenAuthentication]
